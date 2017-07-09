f77c6739f5511d0cdb7a4c0827c95a22a772f02c
Gani Georgiev <gani.georgiev@gmail.com>
2017-07-09 15:18:47 +0300

    updated apidoc

--- api/web/doc/api_project.js
+++ api/web/doc/api_project.js
@@ -18,7 +18,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-06-10T21:00:02.177Z",
+    "time": "2017-07-09T12:15:51.400Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
