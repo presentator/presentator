7d76de57335cb0662de9d4d6a64cfd2c3437ae1e
Gani Georgiev <gani.georgiev@gmail.com>
2017-07-10 20:18:40 +0300

    updated german translations

--- common/messages/de-DE/app.php
+++ common/messages/de-DE/app.php
@@ -17,8 +17,8 @@
  * NOTE: this file must be saved in UTF-8 encoding.
  */
 return array(
-    'Auto scale/fit the uploaded screen to the device width.' => 'Auto scale/fit the uploaded screen to the device width.',
-    '2x (Retina) rescale' => '2x (Retina) rescale',
+    'Auto scale/fit the uploaded screen to the device width.' => 'Automatische Skalierung/Passe den Screen an die Breite des Geräts an.',
+    '2x (Retina) rescale' => '2x (Retina) Neuskalierung',
     '320x480 <em>(Landscape)</em>' => '320x480 <em>(Landscape)</em>',
     '320x480 <em>(Portrait)</em>' => '320x480 <em>(Portrait)</em>',
     '375x667 <em>(Landscape)</em>' => '375x667 <em>(Landscape)</em>',
