b8f093d16ca802000ce70e16f26900f0a681cbe0
Gani Georgiev <gani.georgiev@gmail.com>
2018-01-06 21:43:34 +0200

    updated CEmailValidator tests

--- common/tests/unit/components/validators/CEmailValidatorTest.php
+++ common/tests/unit/components/validators/CEmailValidatorTest.php
@@ -22,9 +22,12 @@ class CEmailValidatorTest extends \Codeception\Test\Unit
      */
     public function testValidation()
     {
-        $validator = new CEmailValidator();
+        $this->specify('Validate wrongly formatted emails', function () {
+            $validator = new CEmailValidator([
+                'allowName'     => true,
+                'allowMultiple' => true,
+            ]);
 
-        $this->specify('Validate wrongly formatted emails', function () use ($validator) {
             verify('Validation should not succeed', $validator->validate(''))->false();
             verify('Validation should not succeed', $validator->validate('test@presentator'))->false();
             verify('Validation should not succeed', $validator->validate('test@presentator.io, invalid'))->false();
@@ -33,12 +36,31 @@ class CEmailValidatorTest extends \Codeception\Test\Unit
             verify('Validation should not succeed', $validator->validate('test1@presentator.io, test2@presentator.io, test3@presentator.io,'))->false();
         });
 
-        $this->specify('Validate correctly formatted emails', function () use ($validator) {
+        $this->specify('Validate correctly formatted emails', function () {
+            $validator = new CEmailValidator([
+                'allowName'     => true,
+                'allowMultiple' => true,
+            ]);
+
             verify('Validation should succeed', $validator->validate('test@presentator.io'))->true();
             verify('Validation should succeed', $validator->validate('test1@presentator.io, test2@presentator.io'))->true();
             verify('Validation should succeed', $validator->validate('Lorem Ipsum <test@presentator.io>'))->true();
             verify('Validation should succeed', $validator->validate('test@presentator.io, Lorem Ipsum <test@presentator.io>'))->true();
             verify('Validation should succeed (trim test)', $validator->validate('  test1@presentator.io,    test2@presentator.io,John Doe <test3@presentator.io>'))->true();
+        });
+
+        $this->specify('Validate email address domain restrictions.', function () {
+            $validator = new CEmailValidator([
+                'allowMultiple'  => true,
+                'allowedDomains' => ['example.com', 'test.com']
+            ]);
+
+            verify('Validation should not succeed', $validator->validate('test@presentator.io'))->false();
+            verify('Validation should not succeed', $validator->validate('test@example.io'))->false();
+            verify('Validation should not succeed', $validator->validate('test1@example.com, test2@presentator.io'))->false();
+            verify('Validation should succeed', $validator->validate('test@test.com'))->true();
+            verify('Validation should succeed', $validator->validate('test@example.com'))->true();
+            verify('Validation should succeed', $validator->validate('test1@example.com, test2@test.com'))->true();
         });
     }
 }
