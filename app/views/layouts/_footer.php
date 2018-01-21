30a5613d9683b21fba76f320ff8d098cd2d1fcd2
Gani Georgiev <gani.georgiev@gmail.com>
2018-01-21 21:20:17 +0200

    updated app footer links

--- app/views/layouts/_footer.php
+++ app/views/layouts/_footer.php
@@ -4,10 +4,9 @@ use common\widgets\LanguageSwitch;
 <footer id="page_footer" class="page-footer">
     <div class="links">
         <ul class="separator-list">
-            <li><?= date('Y') ?> Presentator v<?= Yii::$app->params['currentVersion'] ?></li>
-            <li>
-                <?= LanguageSwitch::widget(); ?>
-            </li>
+            <li>Presentator v<?= Yii::$app->params['currentVersion'] ?></li>
+
+            <li><?= LanguageSwitch::widget(); ?></li>
 
             <?php if (!empty(Yii::$app->params['githubUrl'])): ?>
                 <li>
@@ -27,18 +26,18 @@ use common\widgets\LanguageSwitch;
                 </li>
             <?php endif ?>
 
-            <?php if (!empty(Yii::$app->params['supportUsUrl'])): ?>
-            <li>
-                <a href="<?= Yii::$app->params['supportUsUrl'] ?>" class="icon-link heart-link" target="_blank">
-                    <i class="ion ion-heart"></i>
-                    <span class="txt"><?= Yii::t('app', 'Support us') ?></span>
-                </a>
-            </li>
+            <?php if (!empty(Yii::$app->params['supportUrl'])): ?>
+                <li>
+                    <a href="<?= Yii::$app->params['supportUrl'] ?>" class="icon-link heart-link" target="_blank">
+                        <i class="ion ion-heart"></i>
+                        <span class="txt"><?= Yii::t('app', 'Support us') ?></span>
+                    </a>
+                </li>
             <?php endif ?>
         </ul>
     </div>
 
     <?php if (!empty(Yii::$app->params['showCredits'])): ?>
-        <div class="credits"><?= Yii::t('app', 'Crafted by') ?>&nbsp;<a href="http://gani.bg" target="_blank">Gani</a></div>
+        <div class="credits"><?= Yii::t('app', 'Crafted by') ?>&nbsp;<a href="https://gani.bg" target="_blank">Gani</a></div>
     <?php endif; ?>
 </footer>
