e6a920fe4c61fb186d7c425a0b2eb15c9b595abe
Gani Georgiev <gani.georgiev@gmail.com>
2018-04-08 22:38:21 +0300

    disable composer asset plugin and make use of asset-packagist (with fixed yii dependency)

--- common/config/main.php
+++ common/config/main.php
@@ -5,6 +5,10 @@ $params = array_merge(
 );
 
 return [
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'geoip' => [
