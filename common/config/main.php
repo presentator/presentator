c287f05378347aaeb02a1575ed968ef9d83badd8
Gani Georgiev <gani.georgiev@gmail.com>
2018-04-08 20:58:26 +0300

    revert composer asset plugin

--- common/config/main.php
+++ common/config/main.php
@@ -5,10 +5,6 @@ $params = array_merge(
 );
 
 return [
-    'aliases' => [
-        '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
-    ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'geoip' => [
