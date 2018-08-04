191f2445e814687cd0da2f7a99140f2154f621fa
Gani Georgiev <gani.georgiev@gmail.com>
2018-08-04 19:43:44 +0300

    added preload class to the app layouts

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -12,7 +12,7 @@ AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>">
+<html lang="<?= Yii::$app->language ?>" class="preload">
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
