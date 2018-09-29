c41f87f87f105eb04f3104cfaad31d93624e59cb
Gani Georgiev <gani.georgiev@gmail.com>
2018-09-29 20:22:33 +0300

    bumped currentVersion param

--- common/config/params.php
+++ common/config/params.php
@@ -114,7 +114,7 @@ return [
     ],
 
     // the current application running version used for update checks (will be auto changed on update)
-    'currentVersion' => '1.11.1',
+    'currentVersion' => '1.11.2',
 
     // url to a service that checks whether the provided version is the latest one
     'versionCheckUrl' => 'https://presentator.io/downloads/check',
