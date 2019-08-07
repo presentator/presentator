aeadd4aab278572f4cfbb46155ea7b89878eb5e0
Gani Georgiev <gani.georgiev@gmail.com>
2019-08-07 19:30:17 +0300

    added more detailed comment to the sample env params-local files

--- packages/api/environments/dev/config/params-local.php
+++ packages/api/environments/dev/config/params-local.php
@@ -1,9 +1,9 @@
 <?php
-// list with all available application parameters could be found at https://github.com/presentator/presentator-api/tree/master/config/params.php
+// List with all available application parameters could be found at https://github.com/presentator/presentator-api/tree/master/config/params.php
 return [
     'baseStorageUrl'            => 'http://localhost:8081/storage',
     'appUrl'                    => 'http://localhost:8080/#/',
-    'authClientRedirectUri'     => 'http://localhost:8080/#/auth-callback/',
+    'authClientRedirectUri'     => 'http://localhost:8080/#/auth-callback',
     'activationUrl'             => 'http://localhost:8080/#/activate/{token}',
     'passwordResetUrl'          => 'http://localhost:8080/#/reset-password/{token}',
     'emailChangeUrl'            => 'http://localhost:8080/#/change-email/{token}',
@@ -11,7 +11,14 @@ return [
     'projectLinkCommentViewUrl' => 'http://localhost:8080/#/{slug}/prototypes/{prototypeId}/screens/{screenId}?mode=comments&commentId={commentId}',
     'commentViewUrl'            => 'http://localhost:8080/#/projects/{projectId}/prototypes/{prototypeId}/screens/{screenId}?mode=comments&commentId={commentId}',
 
-    // !!! insert secret unique keys in the following (if empty)
+    // !!!
+    // The application init script will try to populate the following keys for you.
+    // If for some reason their value is empty after app initialization,
+    // you must set a unique value for each manually (eg. '652PfPqYpXY2o0FJC18YgBQn6zIeTqDP').
+    //
+    // Also be cautious when changing `storageKeysSalt` after the initial
+    // installation setup because it could results in corrupted storage files path.
+    // !!!
     'storageKeysSalt'        => '',
     'accessTokenSecret'      => '',
     'activationTokenSecret'  => '',
--- packages/api/environments/prod/config/params-local.php
+++ packages/api/environments/prod/config/params-local.php
@@ -1,9 +1,9 @@
 <?php
-// list with all available application parameters could be found at https://github.com/presentator/presentator-api/tree/master/config/params.php
+// List with all available application parameters could be found at https://github.com/presentator/presentator-api/tree/master/config/params.php
 return [
     'baseStorageUrl'            => 'https://example.com/storage',
     'appUrl'                    => 'https://example.com/#/',
-    'authClientRedirectUri'     => 'https://example.com/#/auth-callback/',
+    'authClientRedirectUri'     => 'https://example.com/#/auth-callback',
     'activationUrl'             => 'https://example.com/#/activate/{token}',
     'passwordResetUrl'          => 'https://example.com/#/reset-password/{token}',
     'emailChangeUrl'            => 'https://example.com/#/change-email/{token}',
@@ -13,7 +13,14 @@ return [
     'noreplyEmail'              => 'noreply@example.com',
     'supportEmail'              => 'support@example.com',
 
-    // !!! insert secret unique keys in the following (if empty)
+    // !!!
+    // The application init script will try to populate the following keys for you.
+    // If for some reason their value is empty after app initialization,
+    // you must set a unique value for each manually (eg. '652PfPqYpXY2o0FJC18YgBQn6zIeTqDP').
+    //
+    // Also be cautious when changing `storageKeysSalt` after the initial
+    // installation setup because it could results in corrupted storage files path.
+    // !!!
     'storageKeysSalt'        => '',
     'accessTokenSecret'      => '',
     'activationTokenSecret'  => '',
