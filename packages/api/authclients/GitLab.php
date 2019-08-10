570c2708c81c996863997a05cefa3b180dfd969b
Gani Georgiev <gani.georgiev@gmail.com>
2019-08-10 08:25:13 +0300

    updated gitlab authclient docs

--- packages/api/authclients/GitLab.php
+++ packages/api/authclients/GitLab.php
@@ -4,9 +4,9 @@ namespace presentator\api\authclients;
 use yii\authclient\OAuth2;
 
 /**
- * GitLab allows authentication via GitLab OAuth.
+ * GitLab allows authentication via GitLab OAuth2.
  *
- * In order to use GitLab OAuth you must register your application at <https://gitlab.com/profile/applications>.
+ * In order to use GitLab OAuth2 you must register your application at <https://gitlab.com/profile/applications>.
  *
  * Example application configuration:
  *
@@ -36,24 +36,32 @@ use yii\authclient\OAuth2;
 class GitLab extends OAuth2
 {
     /**
+     * Domain/base url to the GitLab instance.
+     *
      * @var string
      */
     public $domain = 'https://gitlab.com';
 
     /**
      * {@inheritdoc}
+     *
+     * Will be auto prefixed with `$domain` on init.
      */
     public $authUrl = '/oauth/authorize';
 
     /**
      * {@inheritdoc}
+     *
+     * Will be auto prefixed with `$domain` on init.
      */
     public $tokenUrl = '/oauth/token';
 
     /**
      * {@inheritdoc}
+     *
+     * Will be auto prefixed with `$domain` on init.
      */
-    public $apiBaseUrl = '/api/v4/';
+    public $apiBaseUrl = '/api/v4';
 
     /**
      * {@inheritdoc}
@@ -67,6 +75,12 @@ class GitLab extends OAuth2
     {
         parent::init();
 
+        if (!$this->domain) {
+            throw new InvalidConfigException('GitLab Oauth2 domain must be set.');
+        }
+
+        // normalize props
+        $this->domain     = rtrim($this->domain, '/');
         $this->authUrl    = $this->domain . $this->authUrl;
         $this->tokenUrl   = $this->domain . $this->tokenUrl;
         $this->apiBaseUrl = $this->domain . $this->apiBaseUrl;
