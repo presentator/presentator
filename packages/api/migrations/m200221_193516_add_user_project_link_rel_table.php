6f93429ff07c9fae374b27a39fcd539afb05d03f
Gani Georgiev <gani.georgiev@gmail.com>
2020-02-22 20:55:08 +0200

    added UserProjectLinkRel table migration

---
+++ packages/api/migrations/m200221_193516_add_user_project_link_rel_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Creates `UserProjectLinkRel` table.
+ *
+ * @author Gani Georgiev <gani.georgiev@gmail.com>
+ */
+class m200221_193516_add_user_project_link_rel_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%UserProjectLinkRel}}', [
+            'id'            => $this->primaryKey(),
+            'userId'        => $this->integer()->notNull(),
+            'projectLinkId' => $this->integer()->notNull(),
+            'createdAt'     => $this->datetime(),
+            'updatedAt'     => $this->datetime(),
+        ], $tableOptions);
+        $this->addForeignKey('fk_UserProjectLinkRel_to_User', '{{%UserProjectLinkRel}}', 'userId', '{{%User}}', 'id', 'CASCADE', 'CASCADE');
+        $this->addForeignKey('fk_UserProjectLinkRel_to_ProjectLink', '{{%UserProjectLinkRel}}', 'projectLinkId', '{{%ProjectLink}}', 'id', 'CASCADE', 'CASCADE');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%UserProjectLinkRel}}');
+    }
+}
